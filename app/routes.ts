42ab6eae0d0570e16145127645af6e80f65e7280
ntp1210000 <thanhphuong121098@gmail.com>
2025-04-25 15:57:15 +0700

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index('routes/warning.tsx'),
     route('/live', 'routes/index.tsx'),
-    route('home', 'routes/home.tsx')
+    route('home', 'routes/home.tsx'),
     route('verify', 'routes/verify.tsx'),
     route('*', 'routes/not-found.tsx')
 ] satisfies RouteConfig;
